26ac676144aebbb17caba6982b918f52942e7901
MBDMNET <1bmanuelbeltran@gmail.com>
2020-12-29 09:46:12 +0100

    Create Component Employees

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BooksComponent } from './components/books/books.component';
+import { EmployeesComponent } from './components/employees/employees.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent
+    BooksComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule
